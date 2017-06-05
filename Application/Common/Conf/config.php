e49ff8129dc5e5a3192286b3b324ecf96b417f64
春雷 <2453564902@qq.com>
2017-06-05 15:44:02 +0800

    update_config

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,12 +6,12 @@ return array(
     'DEFAULT_ACTION'       =>   'login',
 	/* 数据库设置 */
     'DB_TYPE'          =>  'mysql',      // 数据库类型
-    'DB_HOST'          =>  'localhost',  // 服务器地址
-    'DB_NAME'          =>  'hongru',      // 数据库名
-    'DB_USER'          =>  'hongru',       // 用户名
-    'DB_PWD'           =>  'hongru2017',       // 密码
+    'DB_HOST'          =>  '192.168.1.222',  // 服务器地址
+    'DB_NAME'          =>  'xtwh',      // 数据库名
+    'DB_USER'          =>  'root',       // 用户名
+    'DB_PWD'           =>  '123456',       // 密码
     'DB_PORT'          =>  '3306',       // 端口
-    'DB_PREFIX'        =>  'hr_',        // 数据库表前缀
+    'DB_PREFIX'        =>  'xt_',        // 数据库表前缀
     'SHOW_PAGE_TRACE'  =>  false,        //开启ThinkPHP页面调试
    'URL_MODEL'             =>  2,       // URL访问模式,可选参数0、1、2、3,代表以下四种模式：
    //支付宝支付配置
