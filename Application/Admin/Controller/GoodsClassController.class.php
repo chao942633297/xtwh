427131c6a74446a1020bf91ec338db6c1afd2c8f
吴敬龙 <969815872@qq.com>
2017-06-06 20:05:56 +0800

    商品

--- Application/Admin/Controller/GoodsClassController.class.php
+++ Application/Admin/Controller/GoodsClassController.class.php
@@ -5,13 +5,52 @@ use Think\Controller;
 class GoodsClassController extends Controller {
 	
 	public function index(){
-		echo 22;
-		// $this->display();	
+		$data = M('goodtype')->select();
+		
+		$arr = array('','品牌','乐器','材质');
+		// $this->ajaxReturn($data);
+		foreach ($data as $key => $value) {
+			$data[$key]['type'] = $arr[$data[$key]['type']];
+			$data[$key]['create_at'] = date('Y-m-d',$data[$key]['create_at']);
+			if($data[$key]['update_at']){
+				$data[$key]['update_at'] = date('Y-m-d',$data[$key]['update_at']);
+			}
+			
+		}
+		$this->assign('data',json_encode($data));
+		
+		
+		$this->display();	
 	}
 
 	public function add(){
-		echo 22;
-		// $this->display();	
+		$this->display();
+	}
+
+	public function do_add(){
+	
+		$data['name'] = I('name');
+		$data['type'] = I('type');
+		$data['create_at'] = time();
+		$row = M('goodtype')->where(array('name'=>$data['name']))->select();
+		if($row){			
+			$result['code']	= 0;
+			$result['msg'] = '数据已存在';
+			$this->ajaxReturn($result);	
+			
+		}
+		$r = M('goodtype')->add($data);
+		if($r){	
+			$result['code']	= 1;
+			$result['msg'] = '添加成功';
+			$this->ajaxReturn($result);
+			
+		}else{			
+			$result['code']	= 0;
+			$result['msg'] = '添加成功';
+			$this->ajaxReturn($result);		
+		}
+		
 	}
 
 	public function edit(){
