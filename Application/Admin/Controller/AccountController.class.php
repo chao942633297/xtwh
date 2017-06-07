ec64aecea9b5768bfa26bc70da14af493b80e1f5
春雷 <2453564902@qq.com>
2017-06-07 16:24:35 +0800

    佣金管理--完成

---
+++ Application/Admin/Controller/AccountController.class.php
@@ -0,0 +1,77 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+
+class AccountController extends Controller {
+	/**
+     * Session过期重新定位到登录页面
+     */
+    public function _initialize(){
+        if (!isset($_SESSION['userid'])){
+            $this->error('你还没有登录,请重新登录', U('/Admin/Login/login'));
+        }
+    }
+
+    #佣金列表
+    public function commissionIndex(){
+        $comm = M('backmoney')->field("*,FROM_UNIXTIME(createtime,'%Y-%m-%d %H:%i:%s') as createtime")->select();  //所有佣金记录        
+    	$userid = M('backmoney')->getField("u2id",true);
+        $userid = array_unique($userid);
+        $where["id"] = array("in",$userid);
+    	$userinfo = M('user2')->where($where)->select();   //所有佣金记录的用户
+
+    	foreach ($comm as $k => $v) {
+    		foreach ($userinfo as $k1 => $v1) {
+    			if ($v['u2id'] == $v1["id"]) {
+                    $comm[$k]["nickname"] = $v1["nickname"];
+                    $comm[$k]["phone"] = $v1["phone"]; 
+                }
+    		}
+    	}
+
+        // var_dump($comm);
+        $this->assign("data",json_encode($comm));
+        $this->display();
+    }
+    //查询
+    public function search(){
+        $start = strtotime(I('start'));
+        $end   = strtotime(I('end'));
+        $time = $start.",".$end;
+        $phone = I('phone');
+        if (empty($start) || empty($end) ) {
+            $this->ajaxReturn(["status"=>0,'msg'=>'起始,结束时间不准为空']);
+        }else{
+            $where["createtime"] = array("between",$time); 
+        }
+        if (!empty($phone)) {
+            $where1["phone"] = array("like","%".$phone."%");
+        }
+        $comm = M('backmoney')->field("*,FROM_UNIXTIME(createtime,'%Y-%m-%d %H:%i:%s') as createtime")->where($where)->select();  //所有佣金记录   
+
+        if (empty($comm) || $comm == false ) {
+            $comm = [];
+        }else{
+            $userid = M('backmoney')->where($where)->getField("u2id",true);
+            $userid = array_unique($userid);
+            $where1["id"] = array("in",$userid);
+            $userinfo = M('user2')->where($where1)->select();   //所有佣金记录的用户
+            foreach ($comm as $k => $v) {
+                foreach ($userinfo as $k1 => $v1) {
+                    if ($v['u2id'] == $v1["id"]) {
+                        $comm[$k]["nickname"] = $v1["nickname"];
+                        $comm[$k]["phone"] = $v1["phone"]; 
+                    }
+                }
+            } 
+        }
+
+        foreach ($comm as $key => $value) {
+            if ($value["phone"] == "" || empty($value["phone"])) {
+                unset($comm[$key]);
+            }
+        }
+        $this->ajaxReturn(["status"=>1,"data"=>$comm]);       
+    }
+
+}
