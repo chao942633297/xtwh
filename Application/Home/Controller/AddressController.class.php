76224a56a0a790f4c0a48cf5ba1be0ebaf482214
贾建超 <942633297@qq.com>
2017-06-12 09:10:51 +0800

    收货地址

---
+++ Application/Home/Controller/AddressController.class.php
@@ -0,0 +1,76 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class AddressController extends Controller{
+
+
+    public function addAddr(){        //新增/修改收货地址   修改地址需传入地址id(addrId). 添加地址 需传入收货人姓名name,收货人手机号phone,省份province, 城市city,区/县area ,
+        $input = I('get.');            //街道 street, 邮编zipcode
+        $address = D('address');
+        $userId = session('home_user_id');
+        $addrId = $input['addrId'];
+        if($input['name']){
+            if(!$address->create($input)){
+                jsonpReturn('1',$address->getError());
+            }
+            if($addrId){
+                $res = $address->where(array('id'=>$addrId))->save();
+            }else{
+                $res = $address->add();
+            }
+            if($res){
+                jsonpReturn('1','成功',$res);
+            }else{
+                jsonpReturn('0','失败');
+            }
+        }else{
+            if($addrId){
+                $addrData = $address->where(array('id'=>$addrId))->find();
+                if($addrData){
+                    jsonpReturn('1','查询成功',$addrData);
+                }
+            }else{
+                jsonpReturn('0','查询失败');
+            }
+        }
+
+    }
+
+    public function delAddr(){  //删除收货地址     需传入地址id(addrId)
+        $input = I('get.');
+        $address = D('Address');
+        if($input){
+            $addrId = $input['addrId'];
+            $res = $address->where(array('id'=>$addrId))->delete();
+            if($res){
+                jsonpReturn('1','删除成功');
+            }else{
+                jsonpReturn('0','删除失败');
+            }
+        }else{
+            jsonpReturn('0','缺少主键');
+        }
+    }
+
+    public function setDefault(){   //设置默认地址   需传入地址id(addrId)
+        $input = I('get.');
+        $address = D('Address');
+        if($input){
+            $addrId = $input['addrId'];
+            $res = $address->where(array('id'=>$addrId))->setField('default','1');
+            if($res){
+                jsonpReturn('1','设置成功',$res);
+            }else{
+                jsonpReturn('0','设置失败');
+            }
+        }else{
+            jsonpReturn('0','缺少主键');
+        }
+    }
+
+
+
+}
+
+
+?>
