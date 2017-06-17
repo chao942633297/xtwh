47741be1a7653f2c33339b6244ec102d1b032f69
配角 <1171119472@qq.com>
2017-06-17 17:14:50 +0800

    用户中心完善

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -7,10 +7,13 @@ class UserController extends Controller{
     # 定义控制器使用表名
     const USERID = 'userid';    //登录的用户id
     const USER    = 'user2';        //用户表
+    const USER1   = 'user1';        //教师培训机构表
     const CHILDREN = 'children';     //子女表
     const BACKMONEY = 'backmoney';  //返佣记录表
     const ORDER = 'order';          //订单表
     const CATEGORY = 'category';    //兴趣类别表
+    const COLLECTION = 'collection';//收藏表
+    const COURSE = 'course';        //课程表
     public function login_test(){
         session('userid',1);
     }
@@ -40,7 +43,7 @@ class UserController extends Controller{
                 $return['grade'] = '合伙人';
                 break;
         }
-        $parent = M(self::USER)->where('id='.$user['pid'])->find();
+        $parent = M(self::USER)->where('id='.$user['refereeid'])->find();
 
         $return['headimg'] = $user['headimg'];
         $return['name'] = $user['name'];
@@ -51,10 +54,10 @@ class UserController extends Controller{
             $return['p_phone'] = $parent['phone'];
         }
         $return['onemoney'] = $user['onemoney'];
-        $return['twomoney'] = $user['twomoney'];
+        // $return['twomoney'] = $user['twomoney'];
         $childen = '';
         for($i = 1;$i <= 3;$i++){
-            $childen .= getChilden($id,$i).',';
+            $childen    .= getChilden($id,$i).',';
         }
         $return['childennum'] = count(explode(',',trim($childen,','))); //下级人数
         $return['backmoney'] = M(self::BACKMONEY)->where(['u2id'=>$id])->sum('money');//教育基金
@@ -70,7 +73,7 @@ class UserController extends Controller{
         $return['headimg'] = $user['headimg'];
         $return['name'] = $user['name'];
         $return['phone'] = $user['phone'];
-        $parent = M(self::USER)->where('id='.$user['pid'])->find();
+        $parent = M(self::USER)->where('id='.$user['refereeid'])->find();
         if($parent){
             $return['p_name'] = $parent['name'];
             $return['p_nickname'] = $parent['nickname'];
@@ -110,7 +113,7 @@ class UserController extends Controller{
         $my['sex'] = $forms['sex'];
         $my['birthday'] = $forms['birthday'];
         M(self::USER)->save($my);
-        $child_info = M(self::CHILDREN)->where(['pid'=>$user['id']])->find();
+        $child_info = M(self::CHILDREN)->where(['fid'=>$user['id']])->find();
         if($child_info){
             $child['id'] = $child_info['id'];
             $child['grade'] = $forms['grade'];
@@ -126,7 +129,7 @@ class UserController extends Controller{
         $id = session(self::USERID);
         $user = M(self::USER)->where(['id'=>$id])->find();
         $return['money']['onemoney'] = $user['onemoney'];
-        $return['money']['twomoney'] = $user['twomoney'];
+        // $return['money']['twomoney'] = $user['twomoney'];
 
         $order = M(self::ORDER)->where(['u2id'=>$id,'status'=>['neq','1']])->order('id desc')->select();
         $return['order'] = $order;
@@ -135,8 +138,23 @@ class UserController extends Controller{
 
     //====================我的余额----余额充值========================
     public function addmoney(){         //须完善
-        //需要传来充值类型    一类：one   二类：two
+        //需要传来充值金额      支付类型  '微信支付','支付宝支付'
+        $id = session(self::USERID);
+        $money = I('money');
+        $paytype = I('paytype');
+        //添加订单
+        $add['ordercode'] = createCode();
+        $add['u2id'] = $id;
+        $add['money'] = $money;
+        $add['status'] = '1';
+        $add['message'] = '余额充值';
+        $add['paytype'] = $paytype;
+        $add['create_at'] = time();
 
+        $order = M(self::ORDER)->add($add);
+        if($order){
+
+        }
     }
 
     //=====================教育基金=========================
@@ -273,13 +291,13 @@ class UserController extends Controller{
             $myorder['ordercode'] = createCode();
             $myorder['u2id'] = $id;
             $myorder['money'] = $money;
-            if($type == 'one'){
+            // if($type == 'one'){
                 $myorder['message'] = '直营余额充值';
                 M(self::USER)->save(['onemoney'=>$my['onemoney']+$money]);
-            }else{
-                $myorder['message'] = '非直营余额充值';
-                M(self::USER)->save(['twomoney'=>$my['twomoney']+$money]);
-            }
+            // }else{
+            //     $myorder['message'] = '非直营余额充值';
+            //     M(self::USER)->save(['twomoney'=>$my['twomoney']+$money]);
+            // }
             $myorder['paytype'] = '基金余额充值';
             $myorder['create_at'] = time();
             $myid = M(self::ORDER)->add($myorder);  //添加订单
@@ -292,15 +310,95 @@ class UserController extends Controller{
 
     //我的伙伴
     public function myPartner(){
+        $id = session(self::USERID);
+        $children = '';
+        for($i = 1;$i <= 3;$i++){
+            $children .= getChilden($id,$i).',';
+        }
+        $children = explode(',',trim($children,','));
+        $return = array();
+        foreach($children as $k=>$v){
+            $user = M(self::USER)->where(['id'=>$v])->find();
+            // dump($user);exit;
+            $return[$k]['name'] = $user['name'];
+            $return[$k]['nickname'] = $user['nickname'];
+            $return[$k]['phone'] = $user['phone'];
+            $return[$k]['create_at'] = $user['create_at'];
+            $return[$k]['headimg'] = $user['headimg'];
+            $return[$k]['grade'] = $user['grade'];
+        }
+
+        dump($return);
+    }
+
+    //我的预存
+    public function myStored(){
 
     }
 
+    //我的课程
+    public function myCourse(){
+        $id = session(self::USERID);
+        $order = M(self::ORDER)->where(['u2id'=>$id,'courseid'=>['neq',0],'status'=>['neq',1]])->select();
+        foreach($order as $k=>$v){
+            $course = M(self::COURSE)->where(['id'=>$v['courseid']])->find();
+            $class = M(self::USER1)->where(['class'=>2,'id'=>$course['user_id']])->find();
+            if($class){
+                $return['course'][$k]['user1'] = $class['title'];
+            }else{
+                $return['course'][$k]['user1'] = '平台';
+            }
+            $return['collection'][$k]['status'] = $course['status'];
+            $return['course'][$k]['title'] = $course['title'];
+            $return['course'][$k]['start_time'] = date('Y-m-d H:i:s',$course['start_time']);
+            $return['course'][$k]['click'] = $course['click'];
+            $return['course'][$k]['line'] = $course['line'];
+            $return['course'][$k]['logo'] = $course['logo'];
+            $return['collection'][$k]['id'] = $course['id'];
+        }
+        dump($return);
 
+    }
 
+    //我的直播
+    public function myLive(){
+
+    }
+
+    //提现详情
+    public function moneyDetail(){
+        $id = session(self::USERID);
+        $return['backmoney'] = M(self::BACKMONEY)->where(['u2id'=>$id])->sum('money');
+        $return['detail'] = M(self::ORDER)->where(['u2id'=>$id,'message'=>'基金提现'])->select();
+        dump($return);
+    }
+
+    //我的收藏
+    public function myCollection(){
+        $id = session(self::USERID);
+        $collection = M(self::COLLECTION)->where(['u2id'=>$id,'courseid'=>['neq',0],'status'=>['neq',1]])->select();
+        $return = array();
+        foreach($controller as $k=>$v){
+            $course = M(self::COURSE)->where(['id'=>$v['courseid']])->find();
+            $class = M(self::USER1)->where(['class'=>2,'id'=>$course['user_id']])->find();
+            if($class){
+                $return['course'][$k]['user1'] = $class['title'];
+            }else{
+                $return['course'][$k]['user1'] = '平台';
+            }
+            $return['collection'][$k]['status'] = $course['status'];
+            $return['collection'][$k]['title'] = $course['title'];
+            $return['collection'][$k]['start_time'] = date('Y-m-d H:i:s',$course['logo']);
+            $return['collection'][$k]['click'] = $course['click'];
+            $return['collection'][$k]['id'] = $course['id'];
+            $return['collection'][$k]['line'] = $course['line'];
+        }
+        dump($return);
+    }
 
     public function test(){
-        $user = getTime(time());
-        dump($user);
+        $user = promotion();
+        // dump($user);
     }
 }
 
@@ -312,9 +410,9 @@ function createCode()//生成订单号
 }
 
 /**
- * 获取指定级别后的所有用户
+ * 获取所有下级
  * @param $uid char 要查询下级的用户id
- * @param $num int   要查的级别后
+ * @param $num int
  * @return 查询指定级别后的用户下级
  */
 function getChildenAll($uid,$userall = ''){
@@ -330,7 +428,7 @@ function getChildenAll($uid,$userall = ''){
         array_push($userall, $uid);
     }
 
-    $where['pid'] = ['in',"$uid"];
+    $where['refereeid'] = ['in',"$uid"];
     $userChilden = M('user2')->field('id')->where($where)->select();
     $userChilden = array_column($userChilden, 'id');
 
@@ -353,7 +451,7 @@ function getChildenAll($uid,$userall = ''){
  */
 function getChilden($uid,$num = 1){
 
-    $where['pid'] = ['IN',"$uid"];
+    $where['refereeid'] = ['IN',"$uid"];
     $user1 = M('user2')->where($where)->select();
     $users_id = '';
     foreach($user1 as $k=>$v){
@@ -378,7 +476,7 @@ function getChilden($uid,$num = 1){
 */
 function getPromotion($id){
     $time = getTime(time());
-    $one_children = M('user2')->where(['pid'=>$id,'create_at'=>['between',$time]])->select();
+    $one_children = M('user2')->where(['refereeid'=>$id,'create_at'=>['between',$time]])->select();
     $two_children = explode(',',getChilden($id,2));
     $three_children = explode(',',getChilden($id,3));
     $money = 0;
@@ -415,4 +513,45 @@ function getTime($time){
     $return['stop'] = strtotime($stop);
     return $return;
     // $time =
+}
+
+/**
+* 晋级规则
+* @param $money 订单金额    $id 用户id
+* @return success    fail
+*/
+function promotion($money = 1,$id){
+    if(empty($money)){
+        return 'fail';
+    }
+    if(empty($id)){
+        $id = session('userid');
+    }
+    $id = 14;
+    $money = 900;
+    $children = explode(',',getChilden($id));
+    $child_money = M('user2')->where(['id'=>['in',$children]])->count('rebate_money');
+    $user = M('user2')->where(['id'=>$id])->find();
+    $user['rebate_money'] += $money;
+    $save['rebate_money'] = $user['rebate_money'];
+    if($user['rebate_money'] >= 900 && $user['rebate_money'] < 1800 && $child_money < 36000 || $user['rebate_money'] < 900 && $child_money >= 36000 && $child_money < 72000){
+        $save['grade'] = '1';
+        M('user2')->where(['id'=>$id])->save($save);
+    }elseif($user['rebate_money'] >= 1800 && $user['rebate_money'] < 3600 && $child_money < 36000 || $user['rebate_money'] >= 900 && $user['rebate_money'] < 1800 && $child_money >= 36000 && $child_money < 72000 || $user['rebate_money'] < 900 && $child_money >= 72000 && $child_money < 108000){
+        $save['grade'] = '2';
+        M('user2')->where(['id'=>$id])->save($save);
+    }elseif($user['rebate_money'] >= 3600 && $user['rebate_money'] < 7200 && $child_money < 36000 || $user['rebate_money'] >= 1800 && $user['rebate_money'] < 3600 && $child_money >= 36000 || $user['rebate_money'] >= 900 && $user['rebate_money'] < 1800 && $child_money >= 72000 || $user['rebate_money'] < 900 && $child_money >= 108000){
+        $save['grade'] = '3';
+        M('user2')->where(['id'=>$id])->save($save);
+    }elseif($user['rebate_money'] >= 3600 && $user['rebate_money'] < 7200 && $child_money > 36000){
+        $save['grade'] = '4';
+        M('user2')->where(['id'=>$id])->save($save);
+    }elseif($user['rebate_money'] >= 7200){
+        $save['grade'] = '5';
+        M('user2')->where(['id'=>$id])->save($save);
+    }else{
+        $save['grade'] = '0';
+        M('user2')->where(['id'=>$id])->save($save);
+    }
+    return 'success';
 }
