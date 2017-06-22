cd166341344f9bd445facfc9fb600adac8209aa1
配角 <1171119472@qq.com>
2017-06-22 10:59:58 +0800

    test1

--- Application/Home/Controller/UserController.class.php
+++ Application/Home/Controller/UserController.class.php
@@ -433,6 +433,17 @@ class UserController extends Controller{
     }
     //我的二维码
     public function myScanCode(){
+        vendor("phpqrcode.phpqrcode");
+        $data = 'http://www.baidu.com';
+        // 纠错级别：L、M、Q、H
+        $level = 'L';
+        // 点的大小：1到10,用于手机端4就可以了
+        $size = 4;
+        // 下面注释了把二维码图片保存到本地的代码,如果要保存图片,用$fileName替换第二个参数false
+//        $path = "images/";
+        // 生成的文件名
+        //$fileName = $path.$size.'.png';
+        QRcode::png($data, false, $level, $size);
 		
     }
     //提现详情
