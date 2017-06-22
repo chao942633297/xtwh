0abded7e350275364207df6d632da67a4c949b17
贾建超 <942633297@qq.com>
2017-06-22 09:53:04 +0800

    微信支付openid

--- Application/Home/Controller/WechatController.class.php
+++ Application/Home/Controller/WechatController.class.php
@@ -23,21 +23,22 @@ class WechatController extends Controller {
 		public function wechatPay(){       //微信支付
 			$orderId = I('get.orderId');
 //			$orderId = '17';
-			$order = D('Order' );
-			$orderData = $order->where(array('id'=>$orderId))->find();
-			$openid ='oZpwf01K2-n9c7v9wH1Tg_sb4bRc';
+			$order = D('OrderRelation');
+			$orderData = $order->where(array('id'=>$orderId))->relation('user2')->find();
 			$jsApi = new JsApi_pub();
-//			if (!isset($_GET['code'])){
-//				//触发微信返回code码
-//				$url = $jsApi->createOauthUrlForCode(WxPayConf_pub::JS_API_CALL_URL);
-//				Header("Location: $url");
-//			}else{
-//				//获取code码，以获取openid
-//				$code = $_GET['code'];
-//				dump($code);die;
-//				$jsApi->setCode($code);
-//				$openid = $jsApi->getOpenId();
-//			}
+			$openid =$orderData['user2']['openid'];
+			if(empty($openid)) {
+				if (!isset($_GET['code'])) {
+					//触发微信返回code码
+					$url = $jsApi->createOauthUrlForCode(WxPayConf_pub::JS_API_CALL_URL);
+					Header("Location: $url");
+				} else {
+					//获取code码，以获取openid
+					$code = $_GET['code'];
+					$jsApi->setCode($code);
+					$openid = $jsApi->getOpenId();
+				}
+			}
 			$unifiedOrder = new UnifiedOrder_pub();
 			$unifiedOrder->setParameter("openid","$openid");//商品描述
 			$unifiedOrder->setParameter("body","杏坛文化");//商品描述
